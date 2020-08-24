84694128ecb267aa8415cd74b6fc2f404610651a
bnoushin7 <noushin7@vt.edu>
2020-08-24 15:47:29 -0400

    issue 83 was resolved

--- development-codes/generate_catalog.py
+++ development-codes/generate_catalog.py
@@ -9,16 +9,13 @@ import os
 @click.command()
 @click.argument('file_path_name')
 @click.argument('dataset_sub_name')
-@click.argument('parent_page')
 @click.argument('tags')
 
-def generate_catalog(file_path_name, dataset_sub_name, parent_page, tags):
+def generate_catalog(file_path_name, dataset_sub_name, tags):
     """
     FILE_NAME: If there are more than one file, FILE_NAME is the pattern for the NetCDF files, otherwise, Name of the NetCDF file. e.g.: 'air.mon.mean.nc' 
 
     DATASET_SUB_NAME: Name of the directory containing the NetCDf data files, e.g.: 'GHCN_CAMS'. If there is subdirectory like monthly, daily, etc., it should also be included and separated by "_".
-
-    PARENT_PAGE: Name of the parent directory in the dataset type hierarchy, e.g.: Temperature
 
     TAG: A dataset may need to be catalogued into multiple child catalogs, e.g.: "Atmosphere", "Temperature". Please keep the format consistent
     """
@@ -26,7 +23,6 @@ def generate_catalog(file_path_name, dataset_sub_name, parent_page, tags):
     path, fileName = os.path.split(file_path_name)
     print("1 :"+ file_path_name)
     print("2 :"+ dataset_sub_name)
-    print("3 :"+ parent_page)
     print("4: "+ tags)
     nfiles = len(glob.glob(file_path_name))
     # Set is_combine based on number of files
@@ -43,7 +39,6 @@ def generate_catalog(file_path_name, dataset_sub_name, parent_page, tags):
 
     #print("dataset_sub_name is "+ dataset_sub_name)
 
-    #print("parent page is " + parent_page)
 
     if int(is_combine) == True:
         # Read with xarray
@@ -85,7 +80,7 @@ def generate_catalog(file_path_name, dataset_sub_name, parent_page, tags):
     # Here url roles as the location
     url = path
     html_repr =xr.core.formatting_html.dataset_repr(src).replace('\\n', '\n')
-    _header = src_header(title, parent_page,  open_catalog, url, tags, open_catalog)
+    _header = src_header(title,  open_catalog, url, tags, open_catalog)
 
     tags =tags.split(',')
     _footer = src_footer()
