3347bf9fca871d531d6a1173253c433bd01542b7
bnoushin7 <noushin7@vt.edu>
2021-05-27 01:00:50 -0400

    forecast/prsfc/daily/anoms/ECCC-GEM/ done

--- development-codes/fake_framework.py
+++ development-codes/fake_framework.py
@@ -40,6 +40,7 @@ def src_header(title, ancestors, open_catalog):
         <ol class="breadcrumb">
 
             <li><a href="main">main</a></li>
+            <li><a href="model">model</a></li>
 
 			{2}
 
